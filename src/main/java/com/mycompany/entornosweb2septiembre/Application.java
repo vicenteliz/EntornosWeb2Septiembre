e963dd58f6a905a99681720dc9ee3227eea6bb84
VicenteLizandra <vicentury@gmail.com>
2021-09-08 20:08:39 +0200

    Subiendo application

---
+++ src/main/java/com/mycompany/entornosweb2septiembre/Application.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.entornosweb2septiembre;
+
+/**
+ *
+ * @author Vicente
+ */
+public class Application {
+
+  
+    public static void main(String[] args) {
+        
+        System.out.println("Aplicacion restaurante");
+    }
+    
+}
